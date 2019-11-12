9974edbe4313695defb04980c78206094710c294
gaoly <shuiruohanyu@foxmail.com>
2019-11-12 16:39:19 +0800

    黑马头条PC-初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
